0e79cc417618d9c13644554898e2c00cc86889fe
kathrin-kleinhammer <kleinhammer@otaris.de>
2020-10-23 16:20:05 +0200

    use environment variable for connectionJson privateKey and adminCert

--- src/main/java/de/nutrisafe/Main.java
+++ src/main/java/de/nutrisafe/Main.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Map;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @PropertySource("classpath:application.properties")
@@ -16,17 +17,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Main implements WebMvcConfigurer {
 
     public static String mspId = "DeoniMSP";
-    public static String connectionJson = "C:\\NutriSafe\\Zertifikate\\connection_deoni.json";
-    public static String privateKey = "C:\\NutriSafe\\Zertifikate\\deoni_private_key";
-    public static String adminCert = "C:\\NutriSafe\\Zertifikate\\deoni_admin_cert.pem";
+    public static String connectionJson;
+    public static String privateKey;
+    public static String adminCert;
 
     public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        for(String e : env.keySet()){
+            switch (e) {
+                case "CONNECTION_JSON" -> connectionJson = env.get(e);
+                case "PRIVATE_KEY" -> privateKey = env.get(e);
+                case "ADMIN_CERT" -> adminCert = env.get(e);
+            }
+        }
         if (args.length > 2){
             mspId = args[0];
             connectionJson = args[1];
             privateKey = args[2];
             adminCert = args[3];
         }
+
         System.out.println("Starting REST API with the following configurations " +
                 " \n MSP ID: " + mspId +
                 " \n Connection path: " + connectionJson +

56a7d7bcda80a8ecbdfb69022e53af6fd70dac4f
kk-ota <kleinhammer@otaris.de>
2020-11-06 10:07:39 +0100

    improved test data creation

--- src/main/resources/test_init.sql
+++ src/main/resources/test_init.sql
@@ -1,1 +1,6 @@
+insert into users(username, password, enabled) values ('testUser', '$2a$10$3P1Eg4fRVELG/Zn7JIYxs.ryDu.bSsZ1EhdcgMSVm1Jw0fRjtOzu2', 't');
+insert into user_to_whitelist(username, whitelist) values ('testUser', 'DEFAULT_READ_WHITELIST');
+insert into user_to_whitelist(username, whitelist) values ('testUser', 'DEFAULT_WRITE_WHITELIST');
+insert into authorities(username, authority) values ('testUser', 'ROLE_MEMBER');
+insert into authorities(username, authority) values ('testUser', 'ROLE_USER');
 insert into whitelist(name) values ('TEST_WHITELIST');
--- src/test/java/NutriSafeRestControllerTest.java
+++ src/test/java/NutriSafeRestControllerTest.java
@@ -2,9 +2,7 @@ import com.google.gson.Gson;
 import de.nutrisafe.NutriSafeRestController;
 import de.nutrisafe.UserDatabaseConfig;
 import de.nutrisafe.jwt.JwtTokenProvider;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -13,28 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.PreparedStatement;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
-
-import static de.nutrisafe.UserDatabaseConfig.ROLE_MEMBER;
-import static de.nutrisafe.UserDatabaseConfig.ROLE_USER;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,14 +30,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class NutriSafeRestControllerTest {
 
     @Autowired
-    private UserDetailsManager userDetailsManager;
-    @Autowired
     private MockMvc mockMvc;
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
 
     private HashMap<Object, Object> body = new HashMap<>();
     private String token;
@@ -69,22 +46,14 @@ public class NutriSafeRestControllerTest {
         // create token
         token = jwtTokenProvider.createToken("admin", Collections.singletonList("ROLE_ADMIN"));
 
-        // create test user
+        // create test user data
         username = "testUser";
         whitelist = "TEST_WHITELIST";
         password  = "12345678";
-
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(ROLE_USER));
-        authorities.add(new SimpleGrantedAuthority(ROLE_MEMBER));
-        UserDetails user = new org.springframework.security.core.userdetails.User(username,
-                new BCryptPasswordEncoder().encode(password), authorities);
-        userDetailsManager.createUser(user);
     }
 
     @AfterEach
     public void cleanup() {
-        userDetailsManager.deleteUser(username);
         body.clear();
     }
 
@@ -215,19 +184,9 @@ public class NutriSafeRestControllerTest {
     }
 
     @Test
-    //@Sql(scripts = "classpath:user_to_whitelist.sql", executionPhase = BEFORE_TEST_METHOD)
-    //@Sql(scripts = "classpath:user_off_whitelist.sql", executionPhase = AFTER_TEST_METHOD)
+    @Sql("classpath:test_init.sql")
+    @Sql("classpath:user_to_whitelist.sql")
     public void linkUserToWhitelistFail_alreadyLinked() throws Exception {
-        PreparedStatementCreator whitelistInsertStatement = connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into " +
-                    "user_to_whitelist(username, whitelist) " +
-                    "values (?, ?)");
-            preparedStatement.setString(1, username);
-            preparedStatement.setString(2, whitelist);
-            return preparedStatement;
-        };
-        jdbcTemplate.update(whitelistInsertStatement);
-
         body.put("username", username);
         body.put("whitelist", whitelist);
         Gson gson = new Gson();
@@ -235,9 +194,6 @@ public class NutriSafeRestControllerTest {
         mockMvc.perform(post("/submit?function=linkUserToWhitelist")
                 .header("Authorization", "Bearer " + this.token).content(json)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().is4xxClientError());
-        mockMvc.perform(post("/submit?function=unlinkUserFromWhitelist")
-                .header("Authorization", "Bearer " + this.token).content(json)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk());
     }
 
     @Test
@@ -265,7 +221,7 @@ public class NutriSafeRestControllerTest {
     @Test
     public void unlinkUserFromWhitelistFail_wlDoesNotExist() throws Exception {
         body.put("username", username);
-        body.put("whitelist", "TEST_WHITELIST1234");
+        body.put("whitelist", "NO_WHITELIST");
         Gson gson = new Gson();
         String json = gson.toJson(body);
         mockMvc.perform(post("/submit?function=unlinkUserFromWhitelist")
@@ -349,19 +305,14 @@ public class NutriSafeRestControllerTest {
     }
 
     @Test
-    //@Sql(scripts = "classpath:user_off_whitelist.sql", executionPhase = AFTER_TEST_METHOD)
     public void setRoleSuccess() throws Exception {
         body.put("username", username);
         body.put("role", "ROLE_ADMIN");
-        body.put("whitelist", "DEFAULT_ADMIN_WHITELIST");
         Gson gson = new Gson();
         String json = gson.toJson(body);
         mockMvc.perform(post("/submit?function=setRole")
                 .header("Authorization", "Bearer " + this.token)
                 .content(json)
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk());
-        mockMvc.perform(post("/submit?function=unlinkUserFromWhitelist")
-                .header("Authorization", "Bearer " + this.token).content(json)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk());
     }
 
